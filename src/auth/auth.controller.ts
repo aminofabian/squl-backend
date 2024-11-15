08df4336f9fe7972b252c1821c2a79957a9da7ea
Fabian Amino <aminofab@gmail.com>
2024-11-15 17:56:32 +0300

    created signup backend

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,15 +1,6 @@
-import {
-  Controller,
-  Get,
-  Post,
-  Body,
-  Patch,
-  Param,
-  Delete,
-} from '@nestjs/common';
+import { Controller, Post, Body } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { CreateUserDto } from '../user/dto/create-user.dto';
-import { UpdateAuthDto } from '../user/dto/update-auth.dto';
 
 @Controller('auth')
 export class AuthController {
@@ -18,25 +9,5 @@ export class AuthController {
   @Post('signup')
   register(@Body() createUserDto: CreateUserDto) {
     return this.authService.registerUser(createUserDto);
-  }
-
-  @Get()
-  findAll() {
-    return this.authService.findAll();
-  }
-
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.authService.findOne(+id);
-  }
-
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateAuthDto: UpdateAuthDto) {
-    return this.authService.update(+id, updateAuthDto);
-  }
-
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.authService.remove(+id);
   }
 }
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,26 +1,15 @@
 import { Injectable } from '@nestjs/common';
 import { CreateUserDto } from '../user/dto/create-user.dto';
-import { UpdateAuthDto } from '../user/dto/update-auth.dto';
+import { UserService } from 'src/user/user.service';
 
 @Injectable()
 export class AuthService {
+  constructor(private readonly userService: UserService) {}
   registerUser(createUserDto: CreateUserDto) {
-    return `This action adds a new auth with data: ${JSON.stringify(createUserDto)}`;
-  }
-
-  findAll() {
-    return `This action returns all auth`;
-  }
-
-  findOne(id: number) {
-    return `This action returns a #${id} auth`;
-  }
-
-  update(id: number, updateAuthDto: UpdateAuthDto) {
-    return `This action updates a #${id} auth with data: ${JSON.stringify(updateAuthDto)}`;
-  }
-
-  remove(id: number) {
-    return `This action removes a #${id} auth`;
+    const user = this.userService.finByEmail(createUserDto.email);
+    if (user) {
+      throw new Error('User already exists');
+    }
+    return this.userService.create(createUserDto);
   }
 }
